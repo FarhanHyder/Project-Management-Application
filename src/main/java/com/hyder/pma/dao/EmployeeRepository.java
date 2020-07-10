38599fdb62abb57c931732825ff051cd1825b7a1
Farhan Hyder <fmshyder@gmail.com>
2020-07-10 16:59:09 -0400

    home page added

--- src/main/java/com/hyder/pma/dao/EmployeeRepository.java
+++ src/main/java/com/hyder/pma/dao/EmployeeRepository.java
@@ -1,10 +1,13 @@
 package com.hyder.pma.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
 import com.hyder.pma.entities.Employee;
 
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+	@Override
+	public List<Employee> findAll();
 
 }
--- src/main/java/com/hyder/pma/dao/ProjectRepository.java
+++ src/main/java/com/hyder/pma/dao/ProjectRepository.java
@@ -1,5 +1,7 @@
 package com.hyder.pma.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.hyder.pma.entities.Project;
@@ -7,4 +9,6 @@ import com.hyder.pma.entities.Project;
 //using this we can do crud data into the database
 public interface ProjectRepository extends CrudRepository<Project, Long>{
 	
+	@Override
+	public List<Project> findAll();
 }
