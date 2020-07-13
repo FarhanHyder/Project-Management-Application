110c98112303005e29c2a7ed2b89b3ca10be3164
Farhan Hyder <fmshyder@gmail.com>
2020-07-13 13:41:03 -0400

    using SpringBootTest for testing

--- src/test/java/com/hyder/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/hyder/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,24 +1,20 @@
-package com.hyder.dao;
+package com.hyder.pma.dao;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.hyder.pma.ProjectManagementApplication;
-import com.hyder.pma.dao.ProjectRepository;
 import com.hyder.pma.entities.Project;
 
-@ContextConfiguration(classes=ProjectManagementApplication.class)
-@RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
+@RunWith(SpringRunner.class)	// JUnit needs this for testing
 @SqlGroup({
     @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"}),
     @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql")
